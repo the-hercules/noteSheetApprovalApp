6200b13616d886075659f57e9515ede32a5f5754
hercules <aashish2535@gmail.com>
2023-03-23 18:47:19 +0530

    minor db changes

--- backend/notesheet_handle/models.py
+++ backend/notesheet_handle/models.py
@@ -2,15 +2,31 @@ from django.db import models
 
 
 # Create your models here
+
+# class AllFacultyUsers(models.Model):
+#     username = models.CharField()
+#     password = models.CharField(max_length=25)
+#     employee_id = models.CharField(max_length=20)
+#     name = models.CharField()
+#     Dob = models.DateField()
+#
+
+# class SenderDetails(models.Model):
+#     sender_id = models.ForeignKey(AllFacultyUsers, on_delete=models.SET_NULL)
+#     sender = models.CharField(max_length=50)
+
 class FacultyDetails(models.Model):
     employee_id = models.CharField(max_length=20)
     Name = models.CharField(max_length=50)
 
 
+# this sender field is tentative
+
 class NoteSheet(models.Model):
     f_id = models.ForeignKey(
-        FacultyDetails, on_delete=models.SET_NULL, null=True)
+        FacultyDetails, on_delete=models.SET_NULL,null=True)
     date = models.DateTimeField(auto_now_add=True)
+    # date = models.DateTimeField(format='%d-%m-%Y %H:%M', auto_now_add=True)
     subject = models.TextField()
     school = models.CharField(max_length=100)
     department = models.CharField(max_length=100)
@@ -19,10 +35,9 @@ class NoteSheet(models.Model):
     proposal_details = models.TextField()
     proposal_submitted_by = models.CharField(max_length=255)
     proposal_submitted_by_1 = models.CharField(max_length=255)
-    name_of_hod = models.CharField(max_length=255)
-    name_of_director = models.CharField(max_length=255)
+    receiver_1 = models.CharField(max_length=255)
+    receiver_2 = models.CharField(max_length=255)
     Status = models.IntegerField(default='000')
-
 
 # class NoteSheetAdditional(models.Model):
 #     f_id = models.ForeignKey(NoteSheet, on_delete=models.SET_NULL, null=True)
