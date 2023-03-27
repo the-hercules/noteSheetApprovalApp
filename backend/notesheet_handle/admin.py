844831c62f766227a5cb3d6c9204793d7b9a4f03
hercules <aashish2535@gmail.com>
2023-03-27 15:30:31 +0530

    minor changes to serializers

--- backend/notesheet_handle/admin.py
+++ backend/notesheet_handle/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
+from .models import NoteSheet, FacultyDetails, AllFacultyUsers
 
 # Register your models here.
+
+admin.site.register(NoteSheet)
+admin.site.register(FacultyDetails)
+admin.site.register(AllFacultyUsers)
--- backend/notesheet_handle/searializers.py
+++ backend/notesheet_handle/searializers.py
@@ -1,14 +1,15 @@
 from rest_framework import serializers
 
-from .models import NoteSheet, FacultyDetails,AllFacultyUsers
+from .models import NoteSheet, FacultyDetails, AllFacultyUsers
 
 
 class NoteSheetSerializer(serializers.HyperlinkedModelSerializer):
     class Meta:
         model = NoteSheet
-        fields = ('url', 'id', 'f_id', 'date', 'school', 'department', 'subject', 'description', 'objective'
-                  , 'proposal_details', 'proposal_submitted_by', 'proposal_submitted_by_1'
-                  , 'receiver_1', 'receiver_2', 'Status')
+        # fields = ('url', 'id', 'f_id', 'date', 'school', 'department', 'subject', 'description', 'objective'
+        #           , 'proposal_details', 'proposal_submitted_by', 'proposal_submitted_by_1'
+        #           , 'receiver_1', 'receiver_2', 'Status')
+        fields = ('__all__')
 
 
 class FacultyDetailsSerializer(serializers.HyperlinkedModelSerializer):
@@ -19,6 +20,7 @@ class FacultyDetailsSerializer(serializers.HyperlinkedModelSerializer):
 
 class AllFacultyUsersSerializer(serializers.HyperlinkedModelSerializer):
     class Meta:
-        model= AllFacultyUsers
-        fields=('username','password','employee_id','first_name','last_name','designation',
-                'school', 'mobile_number','email_address')
+        model = AllFacultyUsers
+        # fields=('username','password','employee_id','first_name','last_name','designation',
+        #         'school', 'mobile_number','email_address')
+        fields = ('__all__')
