acf7fd7cc57b5f14525f9acbfe7cf83703e7c6b3
Sandeep <rebcasandy07@gmail.com>
2023-04-15 16:48:05 +0530

    Added Functions for User in Views.py

--- backend/notesheet_handle/views.py
+++ backend/notesheet_handle/views.py
@@ -8,6 +8,9 @@ from rest_framework.decorators import api_view
 from rest_framework.response import Response
 from rest_framework.views import APIView
 from rest_framework_simplejwt.views import TokenObtainPairView
+from django.db.models import Q
+from .models import NoteSheet, FacultyDetails, AllFacultyUsers
+from .serializers import NoteSheetSerializer, FacultyDetailsSerializer, AllFacultyUsersSerializer, UserSerializer
 
 from .searializers import *
 
@@ -18,15 +21,75 @@ from .searializers import *
 # from rest_framework import permissions
 #
 
-class NoteSheetViewSet(viewsets.ModelViewSet):
-    # class NoteSheetViewSet(generics.ListCreateAPIView):
-    queryset = NoteSheet.objects.all()
-    serializer_class = NoteSheetSerializer
+# class NoteSheetViewSet(viewsets.ModelViewSet):
+#     # class NoteSheetViewSet(generics.ListCreateAPIView):
+#     queryset = NoteSheet.objects.all()
+#     serializer_class = NoteSheetSerializer
 
+
+
+class NoteSheetViewSet(viewsets.ModelViewSet):
+    serializer_class = NoteSheetSerializer
+    #for jwt
+    # authentication_classes = [TokenAuthentication]
+    # permission_classes = [IsAuthenticated]
+
+    def get_queryset(self):
+        user = self.request.user
+        if user.is_superuser:
+            return NoteSheet.objects.all()
+        else:
+            return NoteSheet.objects.filter(Q(proposal_submitted_by=user.username) | Q(receiver_1=user.username) | Q(receiver_2=user.username)).distinct()
+
+class FacultyDetailsViewSet(viewsets.ModelViewSet):
+    serializer_class = FacultyDetailsSerializer
+    # authentication_classes = [TokenAuthentication]
+    # permission_classes = [IsAuthenticated]
+
+    def get_queryset(self):
+        user = self.request.user
+        if user.is_superuser:
+            return FacultyDetails.objects.all()
+        else:
+            return FacultyDetails.objects.filter(employee_id=user.username)
+
+
+class AllFacultyUsersViewSet(viewsets.ModelViewSet):
+    serializer_class = AllFacultyUsersSerializer
+    # authentication_classes = [TokenAuthentication]
+    # permission_classes = [IsAuthenticated]
+
+    def get_queryset(self):
+        user = self.request.user
+        if user.is_superuser:
+            return AllFacultyUsers.objects.all()
+        else:
+            return AllFacultyUsers.objects.filter(username=user.username)
 
 #
 # class FacultyDetailViewSetBYID(viewsets.ModelViewSet):
 # queryset = FacultyProfile.objects.filter(user=)
+
+
+
+@api_view(['GET'])
+# @authentication_classes([TokenAuthentication])
+# @permission_classes([IsAuthenticated])
+def get_current_user(request):
+    serializer = UserSerializer(request.user)
+    return Response(serializer.data)
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 # for auth
